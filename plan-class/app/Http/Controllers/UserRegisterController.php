9fe52a64cd0744ee347e21bd71d3209493227c39
Kamilly <anakamilly9a@gmail.com>
2024-06-24 12:22:58 -0300

    oficialmente acabei

--- plan-class/app/Http/Controllers/UserRegisterController.php
+++ plan-class/app/Http/Controllers/UserRegisterController.php
@@ -6,18 +6,23 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Validation\Rules\Password;
- 
+
 class UserRegisterController extends Controller
 {
     public function store(Request $request)
     {
         Validator::make($request->all(), [
             'name' => ['required', 'string'],
-            'email' => ['required', 'email'],
+            'email' => ['required', 'email', 'unique:users'], 
             'password' => ['required', 'confirmed', Password::min(8)],
         ])->validate();
 
-        $user = User::create($request->all());
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+
         return redirect()->route('login');
     }
 }
